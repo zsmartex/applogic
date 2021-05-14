ee527adad37e8537bb75289806f1b8a289c51c1b
Hữu Hà <business@zsmart.tech>
2021-05-14 10:17:12 +0700

    remove document api

--- src/api/admin/documents/index.cr
+++
@@ -1,9 +0,0 @@
-module API::Admin::Documents
-  class Index < ApiAction
-    get "/api/admin/documents/:id" do
-      document = Document::BaseQuery.find(id).to_json
-
-      json document, status: 200
-    end
-  end
-end
